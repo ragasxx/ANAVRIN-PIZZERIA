fb22176d7b1e4e68eef442c4aa2934192cbe7c20
Sagar Bhatia <sagarbhatia722@gmail.com>
2022-02-17 17:27:16 +0530

    pizza component

---
+++ src/components/Pizza.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
+
+export default function (props) {
+  const { pizza } = props;
+
+  const [quantity, setquantity] = useState(1);
+  const [varient, setvarient] = useState("small");
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <div
+      style={{ margin: "50px" }}
+      className="shadow-lg p-3 mb-5 bg-white rounded"
+    >
+      <div onClick={handleShow}>
+        <h1>{pizza.name}</h1>
+        <img
+          alt={pizza.name}
+          src={pizza.image}
+          className="img-fluid"
+          style={{ height: "200px", width: "200px", borderRadius: "50%" }}
+        />
+      </div>
+
+      <div className="flex-container">
+        <div className="w-100">
+          <p>Varients</p>
+          <select
+            className="mb-3"
+            value={varient}
+            onChange={(e) => {
+              setvarient(e.target.value);
+            }}
+          >
+            {pizza.varients.map((varient) => {
+              return <option value={varient}>{varient}</option>;
+            })}
+          </select>
+        </div>
+
+        <div className="w-100">
+          <p>Quantity</p>
+          <select
+            value={quantity}
+            onChange={(e) => {
+              setquantity(e.target.value);
+            }}
+          >
+            {[...Array(10).keys()].map((x, i) => {
+              return <option value={i + 1}>{i + 1}</option>;
+            })}
+          </select>
+        </div>
+      </div>
+      <div className="flex-container">
+        <div className="m-1 w-100">
+          <p>Price : {pizza.prices[0][varient] * quantity} Rs</p>
+        </div>
+        <div className="m-1 w-100">
+          <button className="btn"> ADD TO CART</button>
+        </div>
+      </div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title> {pizza.name} </Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <img
+            src={pizza.image}
+            className="img-fluid"
+            style={{ height: "300px" }}
+          />
+          <br />
+          <p>{pizza.description}</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <button className="btn" onClick={handleClose}>
+            CLOSE
+          </button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
--- src/components/Pizzas.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Pizzas() {
-  return <div>Pizzas</div>;
-}
