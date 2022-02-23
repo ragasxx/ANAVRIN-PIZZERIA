7c854bb9b74caccf1053f2e9eef5d1f69ba08607
Sagar Bhatia <sagarbhatia722@gmail.com>
2022-02-23 13:30:18 +0530

    created login user

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -17,6 +17,24 @@ export const registerUserReducer = (state = {}, action) => {
         success: action.payload,
       };
 
+    case "USER_LOGIN_REQUEST":
+      return {
+        loading: true,
+      };
+
+    case "USER_LOGIN_SUCCESS":
+      return {
+        loading: false,
+        success: true,
+        currentUser: action.payload,
+      };
+
+    case "USER_LOGIN_FAILED":
+      return {
+        loading: false,
+        error: action.payload,
+      };
+
     default:
       return {
         state,
--- src/screens/Loginscreen.js
+++ src/screens/Loginscreen.js
@@ -1,5 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 
 export default function Loginscreen() {
-  return <div>Loginscreen</div>;
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const dispatch = useDispatch();
+
+  function login() {
+    const user = { email, password };
+
+    // dispatchEvent(loginUser(user));
+    console.log(user);
+  }
+
+  return (
+    <div>
+      <div className="row justify-content-center mt-5">
+        <div className="col-md-5 mt-5 text-left">
+          <h2
+            className="text-center mt-3"
+            style={{
+              fontSize: "32px",
+              fontFamily: "cursive",
+              marginBottom: "30px",
+            }}
+          >
+            LOGIN
+          </h2>
+          <div>
+            <input
+              type="text"
+              required
+              placeholder="Your Email"
+              className="form-control"
+              value={email}
+              onChange={(e) => {
+                setemail(e.target.value);
+              }}
+            />
+            <input
+              type="text"
+              placeholder="Password"
+              required
+              className="form-control"
+              value={password}
+              onChange={(e) => {
+                setpassword(e.target.value);
+              }}
+            />
+            <button className="btn-register mt-2" onClick={login}>
+              LOGIN
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
